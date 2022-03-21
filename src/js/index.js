02361a0cc0f3cafca816d07eecdae2194ad4652f
Júlia Gomes <gomesjulia621@gmail.com>
2022-03-20 22:10:31 -0300

    Inserindo interação na lista e cards com JavaScript

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,24 @@
+const listSelectionPokemons = document.querySelectorAll('.pokemon')
+const pokemonsCard = document.querySelectorAll('card-pokemon')
+ 
+listSelectionPokemons.forEach(pokemon => {
+    pokemon.addEventListener('click', () => {
+        const cardPokemonOpen = document.querySelector('.open')
+        
+        cardPokemonOpen.classList.remove('open')
 
+        const idPokemonSelected = pokemon.attributes.id.value
 
+        const cardPokemonForOpen = document.getElementById('card-' + idPokemonSelected)
+        cardPokemonForOpen.classList.add('open')
 
+        const pokemonListingActive = document.querySelector('.active')
+        pokemonListingActive.classList.remove('active')
 
-
+        const classTypeHover = pokemon.classList[1]
+        const pokemonListingSelected = document.getElementById(idPokemonSelected)
+        pokemonListingSelected.classList.remove(classTypeHover)
+        pokemonListingSelected.classList.add('active') 
+    })
+})
 
